564d58c9b8f01cdc21ec21789c2f435f12b3fe10
jisoo choi <jisoochoi@jisooui-MacBookPro.local>
2022-05-07 15:08:32 +0900

    Django_settings

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sulsajoho',
+        'USER': 'root',
+        'PASSWORD': '0206',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-l)mf8@=u1#x-gp=nh9!-t5=ue&fy471v@$a+f8awl3a6+n+-^l'
